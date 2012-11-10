b8c2d20677860bbfab9cd6ad292079ec5ed330cf
Braden McDorman <bmcdorman@gmail.com>
2012-11-10 12:15:06 -0600

    Added my work so far

--- test/user_example.c
+++ test/user_example.c
@@ -1,4 +1,5 @@
 /* Based on http://people.ee.ethz.ch/~arkeller/linux/kernel_user_space_howto.html#s3 */
+
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <string.h>
@@ -6,55 +7,51 @@
 #define BUFFSIZE 5096 
 
 int main(int argc, char *argv[]) {
-	int sendlen, receivelen;
-	int received = 0;
-	char buffer[BUFFSIZE];
-	struct sockaddr_in receivesocket;
-	struct sockaddr_in sendsocket;
-	int sock;
 	
-	int ret = 0;
-
-	/* Create the UDP socket */
-	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+	int sock = 0;
+	if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
 		perror("socket");
 		return -1;
 	}
 
 	/* my address */
+	struct sockaddr_in receivesocket;
 	memset(&receivesocket, 0, sizeof(receivesocket));  
 	receivesocket.sin_family = AF_INET; 
 	receivesocket.sin_addr.s_addr = htonl(INADDR_ANY);
 	receivesocket.sin_port = htons(9999);
 
-	receivelen = sizeof(receivesocket);
-	if (bind(sock, (struct sockaddr *) &receivesocket, receivelen) < 0) {
+	if(bind(sock, (struct sockaddr *) &receivesocket, sizeof(receivesocket)) < 0) {
 		perror("bind");
-		return -1;		
+		return -1;
 	}
 
 	/* kernel address */
+	struct sockaddr_in sendsocket;
 	memset(&sendsocket, 0, sizeof(sendsocket));
 	sendsocket.sin_family = AF_INET;
 	sendsocket.sin_addr.s_addr = inet_addr("127.0.0.1");
 	sendsocket.sin_port = htons(5555);
 
 	/* Send message to the server */
-	memcpy(buffer, "hello world", strlen("hello world") + 1);
-	sendlen = strlen(buffer) + 1;
+	char buffer[BUFFSIZE];
+	memcpy(buffer, "Hello World", strlen("Hello World") + 1);
+	int sendlen = strlen(buffer) + 1;
 	
-	if (sendto(sock, buffer, sendlen, 0, (struct sockaddr *) &sendsocket, sizeof(sendsocket)) != sendlen) {
+	if(sendto(sock, buffer, sendlen, 0, (struct sockaddr *) &sendsocket, sizeof(sendsocket)) != sendlen) {
 		perror("sendto");
 		return -1;
 	}
 
 	memset(buffer, 0, BUFFSIZE);
-	if ((received = recvfrom(sock, buffer, BUFFSIZE, 0, NULL, NULL)) < 0){
+	int received = 0;
+	if((received = recvfrom(sock, buffer, BUFFSIZE, 0, NULL, NULL)) < 0){
 		perror("recvfrom");
 		return -1;
 	}
+	buffer[BUFFSIZE - 1] = 0;
 	
-	printf("message received: %s\n", buffer);
-
+	printf("Message Received: %s\n", buffer);
+	
 	return 0;
 }
